aa40313b507d6064d599500a2d894ee8319b6a69
GALLA PRUDHVI RAJ <pruney14@PRUNEY.local>
2025-11-17 21:20:42 +0530

    Its done

---
+++ index.js
@@ -0,0 +1,58 @@
+
+let numberOfbuttons = document.querySelectorAll(".drum").length;
+
+for(let i = 0; i<numberOfbuttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        //play audio
+
+        //show animation
+    });
+}
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    //play audio
+
+
+    //show animation
+});
+
+
+
+
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            let tom1 = new Audio("/sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio("/sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio("/sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("/sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("/sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("/sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kickbass = new Audio("/sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+
+        default: console.log("key pressed",key);
+    }
+}
